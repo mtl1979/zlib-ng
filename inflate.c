990a6aa577bead8572c9ef3f4700b41f50f0eceb
Mark Adler <madler@alumni.caltech.edu>
2016-10-11 18:38:20 -0700

    Avoid casting an out-of-range value to long.

--- inflate.c
+++ inflate.c
@@ -1473,7 +1473,7 @@ long ZEXPORT inflateMark(z_stream *strm) {
     struct inflate_state *state;
 
     if (strm == NULL || strm->state == NULL)
-        return -65536;
+        return -(1L << 16);
     state = (struct inflate_state *)strm->state;
     return ((long)(state->back) << 16) + (state->mode == COPY ? state->length :
             (state->mode == MATCH ? state->was - state->length : 0));
