576bbfc5ad1e174227b0a5daa36207348baba94b
Mika Lindqvist <postmaster@raasu.org>
2017-03-21 22:22:10 +0200

    Remove pre-XOP optimized adler32 functions.

--- arch/x86/adler32_x86.c
+++ arch/x86/adler32_x86.c
@@ -10,15 +10,6 @@
 #include "x86.h"
 
 extern uint32_t adler32_c(uint32_t adler, const unsigned char *buf, size_t len);
-#ifdef X86_SSE3_ADLER32
-extern uint32_t adler32_sse3(uint32_t adler, const unsigned char *buf, size_t len);
-#endif
-#ifdef X86_SSSE3_ADLER32
-extern uint32_t adler32_ssse3(uint32_t adler, const unsigned char *buf, size_t len);
-#endif
-#ifdef X86_SSE4_ADLER32
-extern uint32_t adler32_sse4(uint32_t adler, const unsigned char *buf, size_t len);
-#endif
 #ifdef X86_XOP_ADLER32
 extern uint32_t adler32_xop(uint32_t adler, const unsigned char *buf, size_t len);
 #endif
@@ -27,21 +18,6 @@ uint32_t adler32_x86(uint32_t adler, const unsigned char *buf, size_t len) {
 #ifdef X86_XOP_ADLER32
     if (x86_cpu_has_xop) {
         return adler32_xop(adler, buf, len);
-    }
-#endif
-#ifdef X86_SSE4_ADLER32
-    if (x86_cpu_has_sse41) {
-        return adler32_sse4(adler, buf, len);
-    }
-#endif
-#ifdef X86_SSSE3_ADLER32
-    if (x86_cpu_has_ssse3) {
-        return adler32_ssse3(adler, buf, len);
-    }
-#endif
-#ifdef X86_SSE3_ADLER32
-    if (x86_cpu_has_sse3) {
-        return adler32_sse3(adler, buf, len);
     }
 #endif
 
