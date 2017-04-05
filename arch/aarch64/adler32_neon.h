23b33ea2e4d937a5fbc39288d13f1f99740fd976
Adenilson Cavalcanti <adenilson.cavalcanti@arm.com>
2017-04-05 16:19:37 -0700

    Handling the case of linaro compilers + linux.
    Conflicts:
    	arch/aarch64/adler32_neon.h
    	arch/arm/adler32_neon.h

--- arch/aarch64/adler32_neon.h
+++ arch/aarch64/adler32_neon.h
@@ -20,7 +20,11 @@
 #define __ADLER32_NEON__
 
 #if (defined(__ARM_NEON__) || defined(__ARM_NEON))
+// Depending on the compiler flavor, size_t may be defined in
+// one or the other header. See:
+// http://stackoverflow.com/questions/26410466/gcc-linaro-compiler-throws-error-unknown-type-name-size-t
 #include <stdint.h>
+#include <stddef.h>
 uint32_t adler32_neon(uint32_t adler, const unsigned char *buf,
                       size_t len);
 #endif
